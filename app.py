7d567c6e97b53430014559ece831c52e5605a974
JeremyChim <jer888chim@outlook.com>
2023-05-16 02:12:09 +0800

    确认可以跑了

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time ： 2023/5/16 16:42
+# @Auth ： JeremyChim
+# @File ：app.py
+# @IDE ：PyCharm
+# @Github ：https://github.com/JeremyChim
+
+import os
 import function.flier as fl
 
 fl.log_0011()
@@ -8,3 +16,7 @@ fl.log_31B()
 import function.udp146
 import function.udp17F
 import function.udp31B
+
+os.rename('cache/udp_146.txt','UDP_MSG_146.csv')
+os.rename('cache/udp_17F.txt','UDP_MSG_17F.csv')
+os.rename('cache/udp_31B.txt','UDP_MSG_31B.csv')
